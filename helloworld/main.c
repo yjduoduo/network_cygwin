1c038e73925aa5609f36423d5e07f8e568ccf445
yjduoduo <wxjlmr@126.com>
2014-04-28 09:01:55 +0800

    first  hello world test!!!!!!!

---
+++ helloworld/main.c
@@ -0,0 +1,16 @@
+/***********************************************************
+********************main.c test************************
+************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void)
+{
+
+
+	printf("hello,world,xiaoweilai!\n");
+	while(1);
+	return 0;
+}
